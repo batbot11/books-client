8f97b4fa6762f5d917de34c063eeea0999501f5e
batbot11 <40738848+batbot11@users.noreply.github.com>
2019-03-13 10:39:05 +0530

    All issues of signin addressed

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import {Route} from "react-router";
 import {Container} from "semantic-ui-react";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
+import DashboardPage from "./components/pages/DashboardPage";
+import UserRoute from "./components/routes/UserRoute";
+import GuestRoute from "./components/routes/GuestRoute";
 
-const App = () => (
+const App = ({location}) => (
     <Container>
-        <Route path="/" exact component = {HomePage} />
-        <Route path="/login" component = {LoginPage} />
+        <Route location={location} path="/" exact component = {HomePage} />
+        <GuestRoute location={location} path="/login" component = {LoginPage} />
+        <UserRoute location={location} path="/dashboard" component = {DashboardPage} />
     </Container>
 )
 
